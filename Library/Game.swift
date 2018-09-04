84b413149502de1db13cf525a378ca32a9613a90
Jordan <jordan.jones@detroitlabs.com>
2018-09-04 16:23:55 -0400

    Added two model files, a Game model and a GameCollection model

---
+++ Library/Game.swift
@@ -0,0 +1,16 @@
+//
+//  Game.swift
+//  Library
+//
+//  Created by Jordan on 9/4/18.
+//  Copyright © 2018 Jordan Jones. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    
+    var name: String
+    var platform: String
+    
+}
---
+++ Library/GameCollection.swift
@@ -0,0 +1,15 @@
+//
+//  GameCollection.swift
+//  Library
+//
+//  Created by Jordan on 9/4/18.
+//  Copyright © 2018 Jordan Jones. All rights reserved.
+//
+
+import Foundation
+
+struct GameCollection {
+    
+    var gameCollection: [String : String] = [:]
+    
+}
